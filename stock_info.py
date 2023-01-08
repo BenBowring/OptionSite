87c4d2e99b1c584c505fd9df29ece909b7e370b7
BenBowring <benjamin.bowring@gmail.com>
2023-01-08 18:36:39 +0000

    Interpolation Basic Method for delta calc

---
+++ stock_info.py
@@ -0,0 +1,5 @@
+
+
+def get_names():
+
+    # Runs to get list of stock names and associated sectors
---
+++ yahoo.py
@@ -0,0 +1,5 @@
+
+
+def clarify():
+
+    #Decorator that while loops through the output rolling 3 times to make sure it stays consistent
