5eaefe68938e917c66a72ce5a8958ec78fb99ed7
BenBowring <benjamin.bowring@gmail.com>
2023-01-09 02:55:02 +0000

    Incorrect delta output, will need to verify thread

--- yahoo.py
+++ yahoo.py
@@ -3,3 +3,7 @@
 def clarify():
 
     #Decorator that while loops through the output rolling 3 times to make sure it stays consistent
+    
+def ignore_gaps():
+    
+    #Ignore gaps in option chain and other pulls, runs enough times to best guess guarantee not missing values
