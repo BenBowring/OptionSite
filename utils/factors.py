abae25059e311a08d56c911dbc116b9537d1bebe
BenBowring <benjamin.bowring@gmail.com>
2023-06-07 13:25:01 +0100

    Updating it all

---
+++ utils/factors.py
@@ -0,0 +1,4 @@
+# Two directions for this, we can do correlation to factor funds
+# Or we could do a multivariate variation explained model akin to the EUA project
+
+# since we're based in the US this is even easier
---
+++ utils/price_analysis.py
@@ -0,0 +1,2 @@
+# Create functions that do the big trader loved ones, SMA, resistance levels etc
+# Can then just create a grid that chooses ones that are hitting most favoured ones
